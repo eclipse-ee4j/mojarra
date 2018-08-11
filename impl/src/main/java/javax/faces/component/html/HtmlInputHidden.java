1da0900de098e6007ad375909a91a631219fa195
Manfred Riem (Oracle account) <manfredriem@users.noreply.github.com>
2018-08-11 14:57:38 -0600

    Fixes issue #4436 - Remove code generation for javax.faces.component.html (#4438)

---
+++ impl/src/main/java/javax/faces/component/html/HtmlInputHidden.java
@@ -0,0 +1,80 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ * 
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package javax.faces.component.html;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.el.ValueExpression;
+import javax.faces.component.UIInput;
+
+/**
+ * <p>Represents an HTML <code>input</code> element
+ * of type <code>hidden</code>.</p>
+ * <p>By default, the <code>rendererType</code> property must be set to "<code>javax.faces.Hidden</code>".
+ * This value can be changed by calling the <code>setRendererType()</code> method.</p>
+ */
+public class HtmlInputHidden extends UIInput {
+
+
+
+    private static final String OPTIMIZED_PACKAGE = "javax.faces.component.";
+
+    public HtmlInputHidden() {
+        super();
+        setRendererType("javax.faces.Hidden");
+    }
+
+
+    /**
+     * <p>The standard component type for this component.</p>
+     */
+    public static final String COMPONENT_TYPE = "javax.faces.HtmlInputHidden";
+
+
+    protected enum PropertyKeys {
+;
+        String toString;
+        PropertyKeys(String toString) { this.toString = toString; }
+        PropertyKeys() { }
+        public String toString() {
+            return ((toString != null) ? toString : super.toString());
+        }
+}
+
+    private void handleAttribute(String name, Object value) {
+        List<String> setAttributes = (List<String>) this.getAttributes().get("javax.faces.component.UIComponentBase.attributesThatAreSet");
+        if (setAttributes == null) {
+            String cname = this.getClass().getName();
+            if (cname != null && cname.startsWith(OPTIMIZED_PACKAGE)) {
+                setAttributes = new ArrayList<String>(6);
+                this.getAttributes().put("javax.faces.component.UIComponentBase.attributesThatAreSet", setAttributes);
+            }
+        }
+        if (setAttributes != null) {
+            if (value == null) {
+                ValueExpression ve = getValueExpression(name);
+                if (ve == null) {
+                    setAttributes.remove(name);
+                }
+            } else if (!setAttributes.contains(name)) {
+                setAttributes.add(name);
+            }
+        }
+    }
+
+}
