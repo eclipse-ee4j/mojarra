75a7f210aa121d6447df698e75ddf07b916e1d52
Chao Wang <chaowan@redhat.com>
2020-03-04 00:58:13 +0800

    2nd part fix for issue4550, If the component is NOT valid, the associated ValueChangeListener must NOT be invoked
    Signed-off-by: Chao Wang <chaowan@redhat.com>

--- impl/src/main/java/javax/faces/component/UIInput.java
+++ impl/src/main/java/javax/faces/component/UIInput.java
@@ -886,7 +886,7 @@ public class UIInput extends UIOutput implements EditableValueHolder {
             }
         }
 
-        if (compareValues(previous, newValue)) {
+        if (isValid() && compareValues(previous, newValue)) {
             queueEvent(new ValueChangeEvent(context, this, previous, newValue));
         }
 
