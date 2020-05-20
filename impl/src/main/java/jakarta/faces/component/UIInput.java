b054f7a5bfe8c994da2ebd240d2ce97d118662a6
Chao Wang <chaowan@redhat.com>
2020-05-20 13:58:27 +0800

    Revert "Fix problem introduced in JAVASERVERFACES_SPEC_PUBLIC-1329, also consider Submitted value "" when EMPTY_STRING_AS_NULL_PARAM_NAME is enabled."
    This reverts commit b3253335366a1d4171f0e066e75c38adbb0755c9.
    
    Signed-off-by: Chao Wang <chaowan@redhat.com>

--- impl/src/main/java/jakarta/faces/component/UIInput.java
+++ impl/src/main/java/jakarta/faces/component/UIInput.java
@@ -936,9 +936,10 @@ public class UIInput extends UIOutput implements EditableValueHolder {
 
         // If non-null, an instanceof String, and we're configured to treat
         // zero-length Strings as null:
-        // call setSubmittedValue(null)
-        boolean isEmptyStringNull = considerEmptyStringNull(context) && submittedValue instanceof String && ((String) submittedValue).length() == 0;
-        if (isEmptyStringNull) {
+        //   call setSubmittedValue(null)
+        if ((considerEmptyStringNull(context)
+             && submittedValue instanceof String
+             && ((String) submittedValue).length() == 0)) {
             setSubmittedValue(null);
             submittedValue = null;
         }
@@ -959,18 +960,13 @@ public class UIInput extends UIOutput implements EditableValueHolder {
 
         // If our value is valid, store the new value, erase the
         // "submitted" value, and emit a ValueChangeEvent if appropriate
-        Object previous = getValue();
-        if (isValid() && !isEmptyStringNull) {
+        if (isValid()) {
+            Object previous = getValue();
             setValue(newValue);
             setSubmittedValue(null);
-        } else {
-            if (submittedValue == null) {
-                setSubmittedValue("");
+            if (compareValues(previous, newValue)) {
+                queueEvent(new ValueChangeEvent(context, this, previous, newValue));
             }
-        }
-
-        if (compareValues(previous, newValue)) {
-            queueEvent(new ValueChangeEvent(context, this, previous, newValue));
         }
 
     }
