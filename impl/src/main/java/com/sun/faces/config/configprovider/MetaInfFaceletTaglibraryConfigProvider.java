7dbdbc1b27577d33e3a8829d993a753204002bb1
arjantijms <arjan.tijms@gmail.com>
2020-10-26 13:59:27 +0100

    #4769 Remove ancient check for Facelets 1.1.x standalone lib on cp
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

--- impl/src/main/java/com/sun/faces/config/configprovider/MetaInfFaceletTaglibraryConfigProvider.java
+++ impl/src/main/java/com/sun/faces/config/configprovider/MetaInfFaceletTaglibraryConfigProvider.java
@@ -17,13 +17,13 @@
 package com.sun.faces.config.configprovider;
 
 import static com.sun.faces.util.Util.getCurrentLoader;
-import static java.util.Collections.emptyList;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
@@ -35,18 +35,13 @@ import jakarta.faces.FacesException;
 import jakarta.servlet.ServletContext;
 
 /**
+ * This resource providers finds all .taglib.xml files in the system.
  *
  */
 public class MetaInfFaceletTaglibraryConfigProvider implements ConfigurationResourceProvider {
 
     private static final String SUFFIX = ".taglib.xml";
     private static final String WEB_INF_CLASSES = "/WEB-INF/classes/META-INF";
-
-    /**
-     * Array of taglib.xml files included with Facelets 1.1.x. If they are on the classpath, we don't want to process them.
-     */
-    private static final String[] FACELET_CONFIG_FILES = { "META-INF/jsf-core.taglib.xml", "META-INF/jsf-html.taglib.xml", "META-INF/jsf-ui.taglib.xml",
-            "META-INF/jstl-core.taglib.xml", "META-INF/jstl-fn.taglib.xml" };
 
 
     // -------------------------------------------- Methods from ConfigProcessor
@@ -55,71 +50,35 @@ public class MetaInfFaceletTaglibraryConfigProvider implements ConfigurationReso
     public Collection<URI> getResources(ServletContext context) {
 
         try {
-            URL[] externalTaglibUrls = Classpath.search(getCurrentLoader(this), "META-INF/", SUFFIX);
-
-            // Perform some 'correctness' checking. If the user has
-            // removed the FaceletViewHandler from their configuration,
-            // but has left the jsf-facelets.jar in the classpath, we
-            // need to ignore the default configuration resouces from
-            // that JAR.
-            List<URI> urlsList = pruneURLs(externalTaglibUrls);
+            List<URL> resourceURLs = asList(Classpath.search(getCurrentLoader(this), "META-INF/", SUFFIX));
 
             // Special case for finding taglib files in WEB-INF/classes/META-INF
             Set<String> paths = context.getResourcePaths(WEB_INF_CLASSES);
             if (paths != null) {
                 for (String path : paths) {
-                    if (path.endsWith(".taglib.xml")) {
-                        try {
-                            urlsList.add(new URI(context.getResource(path).toExternalForm().replaceAll(" ", "%20")));
-                        } catch (URISyntaxException ex) {
-                            throw new FacesException(ex);
-                        }
+                    if (path.endsWith(SUFFIX)) {
+                        resourceURLs.add(context.getResource(path));
                     }
                 }
             }
 
-            return urlsList;
+            return resourceURLs.stream()
+                               .map(url -> transformToURI(url))
+                               .collect(toList());
+
         } catch (IOException ioe) {
             throw new FacesException("Error searching classpath from facelet-taglib documents", ioe);
         }
-
     }
 
     // --------------------------------------------------------- Private Methods
 
-    private List<URI> pruneURLs(URL[] urls) {
-
-        List<URI> ret = null;
-        if (urls != null && urls.length > 0) {
-            for (URL url : urls) {
-                String u = url.toString();
-                boolean found = false;
-                for (String excludeName : FACELET_CONFIG_FILES) {
-                    if (u.contains(excludeName)) {
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (!found) {
-                    if (ret == null) {
-                        ret = new ArrayList<>();
-                    }
-
-                    try {
-                        ret.add(new URI(url.toExternalForm().replaceAll(" ", "%20")));
-                    } catch (URISyntaxException ex) {
-                        throw new FacesException(ex);
-                    }
-                }
-            }
+    private URI transformToURI(URL url) {
+        try {
+            return new URI(url.toExternalForm().replaceAll(" ", "%20"));
+        } catch (URISyntaxException ex) {
+            throw new FacesException(ex);
         }
-
-        if (ret == null) {
-            ret = emptyList();
-        }
-
-        return ret;
     }
 
 }
