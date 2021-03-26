c8a5d0a5c0d4cf593f12f48df8e299bfb6b0ea79
Manfred Riem <22894110+manorrock@users.noreply.github.com>
2021-03-26 16:08:33 -0600

    Fixes issue #4840

--- cdi/src/main/java/org/eclipse/mojarra/cdi/CdiLifecycleFactory.java
+++ cdi/src/main/java/org/eclipse/mojarra/cdi/CdiLifecycleFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2021 Contributors to Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
 package org.eclipse.mojarra.cdi;
 
 import jakarta.enterprise.inject.literal.NamedLiteral;
@@ -16,13 +31,19 @@ import javax.naming.NamingException;
 
 /**
  * The CDI LifecycleFactory.
+ * 
+ * <p>
+ *  This factory makes it possible for your CDI implementation to determine
+ *  which Lifecycle to use when a Lifecycle is requested.
+ * 
+ * @since 4.0
  */
 public class CdiLifecycleFactory extends LifecycleFactory {
 
     /**
      * Constructor.
      *
-     * @param wrapped the wrapped lifecycle factory.
+     * @param wrapped the wrapped LifecycleFactory.
      */
     public CdiLifecycleFactory(LifecycleFactory wrapped) {
         super(wrapped);
@@ -61,7 +82,7 @@ public class CdiLifecycleFactory extends LifecycleFactory {
      *
      * @return the bean manager.
      */
-    public BeanManager getBeanManager() {
+    private BeanManager getBeanManager() {
         BeanManager beanManager = null;
         try {
             InitialContext initialContext = new InitialContext();
--- cdi/src/main/java/org/eclipse/mojarra/cdi/CdiRenderKitFactory.java
+++ cdi/src/main/java/org/eclipse/mojarra/cdi/CdiRenderKitFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2021 Contributors to Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
 package org.eclipse.mojarra.cdi;
 
 import jakarta.enterprise.inject.literal.NamedLiteral;
@@ -17,6 +32,12 @@ import javax.naming.NamingException;
 
 /**
  * The CDI RenderKitFactory.
+ * 
+ * <p>
+ *  This factory makes it possible for your CDI implementation to determine
+ *  which RenderKit to use when a RenderKit is requested.
+ * 
+ * @since 4.0
  */
 public class CdiRenderKitFactory extends RenderKitFactory {
 
