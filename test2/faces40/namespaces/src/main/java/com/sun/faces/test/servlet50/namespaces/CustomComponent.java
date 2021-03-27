c0dc8c96f5a316c2d6560b5766723939a0724ba4
Bauke Scholtz <balusc@gmail.com>
2021-03-27 14:59:00 -0400

    Implement IT for all namespaces https://github.com/eclipse-ee4j/faces-api/issues/1553

---
+++ test2/faces40/namespaces/src/main/java/com/sun/faces/test/servlet50/namespaces/CustomComponent.java
@@ -0,0 +1,9 @@
+package com.sun.faces.test.servlet50.namespaces;
+
+import jakarta.faces.component.FacesComponent;
+import jakarta.faces.component.UIOutput;
+
+@FacesComponent(createTag = true)
+public class CustomComponent extends UIOutput {
+
+}
