41fa9efc19533617a3d8228e26a73016d6e39ffb
arjantijms <arjan.tijms@gmail.com>
2021-03-28 09:26:05 +0200

    Migrated search expression test to Faces23
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

---
+++ test2/faces23/searchExpression/src/main/java/com/sun/faces/test/javaee8/searchExpression/CustomSearchKeywordResolver.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2017, 2021 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package com.sun.faces.test.javaee8.searchExpression;
+
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.component.search.SearchExpressionContext;
+import jakarta.faces.component.search.SearchKeywordContext;
+import jakarta.faces.component.search.SearchKeywordResolver;
+
+public class CustomSearchKeywordResolver extends SearchKeywordResolver {
+
+    @Override
+    public boolean isResolverForKeyword(SearchExpressionContext searchExpressionContext, String keyword) {
+        return "custom".equals(keyword);
+    }
+
+    @Override
+    public void resolve(SearchKeywordContext searchKeywordContext, UIComponent component, String keyword) {
+        searchKeywordContext.invokeContextCallback(component);
+        searchKeywordContext.setKeywordResolved(true);
+    }
+
+}
