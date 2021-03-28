93c83e1beba5caf368ccc2f4f55fbe8e5cf8e5f9
Bauke Scholtz <balusc@gmail.com>
2021-03-28 08:10:05 -0400

    Add IT for f:selectItemGroup / UISelectItemGroup https://github.com/eclipse-ee4j/faces-api/issues/1563

---
+++ test2/faces40/selectItemGroup/src/main/java/com/sun/faces/test/servlet50/selectitemgroup/Product.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2021 Contributors to Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package com.sun.faces.test.servlet50.selectitemgroup;
+
+public class Product {
+
+    private Long id;
+    private String name;
+
+    public Product() {
+        //
+    }
+
+    public Product(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
