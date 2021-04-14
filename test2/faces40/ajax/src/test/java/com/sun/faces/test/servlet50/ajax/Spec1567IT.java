33e050a2ea1b319c4139f86fbbd3276eba2491b3
Bauke Scholtz <balusc@gmail.com>
2021-04-14 16:37:07 -0400

    IT for improved f:ajax render behavior on composites

--- test2/faces40/ajax/src/test/java/com/sun/faces/test/servlet50/ajax/Spec1567IT.java
+++ test2/faces40/ajax/src/test/java/com/sun/faces/test/servlet50/ajax/Spec1567IT.java
@@ -35,6 +35,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 
@@ -63,12 +64,138 @@ public class Spec1567IT {
     }
 
     @Test
-    public void testAjaxExecute() throws Exception {
+    public void test() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "spec1567IT.xhtml");
 
         validateRenderedMarkup(page, "form1", "form1:messages");
         validateRenderedMarkup(page, "form2", "form2:messages");
         validateRenderedMarkup(page, "form3", "form3:inputs form3:messages");
+
+        // fill form1 input1
+        HtmlTextInput input1 = (HtmlTextInput) page.getElementById("form1:inputs:input1");
+        input1.setValueAttribute("1");
+        input1.fireEvent("change");
+        webClient.waitForBackgroundJavaScript(3000);
+        input1 = (HtmlTextInput) page.getElementById("form1:inputs:input1");
+        HtmlTextInput input2 = (HtmlTextInput) page.getElementById("form1:inputs:input2");
+        HtmlTextInput input3 = (HtmlTextInput) page.getElementById("form1:inputs:input3");
+        String messages = page.getElementById("form1:messages").asText();
+        assertEquals("input1 is filled with 1", "1", input1.getValueAttribute());
+        assertEquals("input2 is empty", "", input2.getValueAttribute());
+        assertEquals("input3 is empty", "", input3.getValueAttribute());
+        assertEquals("input1 is filled and input2 is empty", "setForm1input1:1\nsetForm1input2:", messages);
+
+        // fill form1 input2
+        input2 = (HtmlTextInput) page.getElementById("form1:inputs:input2");
+        input2.setValueAttribute("1");
+        input2.fireEvent("change");
+        webClient.waitForBackgroundJavaScript(3000);
+        input1 = (HtmlTextInput) page.getElementById("form1:inputs:input1");
+        input2 = (HtmlTextInput) page.getElementById("form1:inputs:input2");
+        input3 = (HtmlTextInput) page.getElementById("form1:inputs:input3");
+        messages = page.getElementById("form1:messages").asText();
+        assertEquals("input1 is filled with 1", "1", input1.getValueAttribute());
+        assertEquals("input2 is filled with 1", "1", input2.getValueAttribute());
+        assertEquals("input3 is empty", "", input3.getValueAttribute());
+        assertEquals("input1 is filled and input2 is filled", "setForm1input1:1\nsetForm1input2:1", messages);
+
+        // fill form1 input3
+        input3 = (HtmlTextInput) page.getElementById("form1:inputs:input3");
+        input3.setValueAttribute("1");
+        input3.fireEvent("change");
+        webClient.waitForBackgroundJavaScript(3000);
+        input1 = (HtmlTextInput) page.getElementById("form1:inputs:input1");
+        input2 = (HtmlTextInput) page.getElementById("form1:inputs:input2");
+        input3 = (HtmlTextInput) page.getElementById("form1:inputs:input3");
+        messages = page.getElementById("form1:messages").asText();
+        assertEquals("input1 is refreshed to empty string", "", input1.getValueAttribute());
+        assertEquals("input2 is refreshed to empty string", "", input2.getValueAttribute());
+        assertEquals("input3 is filled and refreshed", "1x", input3.getValueAttribute());
+        assertEquals("input3 is filled", "setForm1input3:1", messages);
+
+        // fill form2 input1
+        input1 = (HtmlTextInput) page.getElementById("form2:inputs:input1");
+        input1.setValueAttribute("1");
+        input1.fireEvent("change");
+        webClient.waitForBackgroundJavaScript(3000);
+        input1 = (HtmlTextInput) page.getElementById("form2:inputs:input1");
+        input2 = (HtmlTextInput) page.getElementById("form2:inputs:input2");
+        input3 = (HtmlTextInput) page.getElementById("form2:inputs:input3");
+        messages = page.getElementById("form2:messages").asText();
+        assertEquals("input1 is filled with 1", "1", input1.getValueAttribute());
+        assertEquals("input2 is empty", "", input2.getValueAttribute());
+        assertEquals("input3 is empty", "", input3.getValueAttribute());
+        assertEquals("input1 is filled and input2 is empty", "setForm2input1:1\nsetForm2input2:", messages);
+
+        // fill form2 input2
+        input2 = (HtmlTextInput) page.getElementById("form2:inputs:input2");
+        input2.setValueAttribute("1");
+        input2.fireEvent("change");
+        webClient.waitForBackgroundJavaScript(3000);
+        input1 = (HtmlTextInput) page.getElementById("form2:inputs:input1");
+        input2 = (HtmlTextInput) page.getElementById("form2:inputs:input2");
+        input3 = (HtmlTextInput) page.getElementById("form2:inputs:input3");
+        messages = page.getElementById("form2:messages").asText();
+        assertEquals("input1 is filled with 1", "1", input1.getValueAttribute());
+        assertEquals("input2 is filled with 1", "1", input2.getValueAttribute());
+        assertEquals("input3 is empty", "", input3.getValueAttribute());
+        assertEquals("input1 is filled and input2 is filled", "setForm2input1:1\nsetForm2input2:1", messages);
+
+        // fill form2 input3
+        input3 = (HtmlTextInput) page.getElementById("form2:inputs:input3");
+        input3.setValueAttribute("1");
+        input3.fireEvent("change");
+        webClient.waitForBackgroundJavaScript(3000);
+        input1 = (HtmlTextInput) page.getElementById("form2:inputs:input1");
+        input2 = (HtmlTextInput) page.getElementById("form2:inputs:input2");
+        input3 = (HtmlTextInput) page.getElementById("form2:inputs:input3");
+        messages = page.getElementById("form2:messages").asText();
+        assertEquals("input1 is refreshed to empty string", "", input1.getValueAttribute());
+        assertEquals("input2 is refreshed to empty string", "", input2.getValueAttribute());
+        assertEquals("input3 is filled and refreshed", "1x", input3.getValueAttribute());
+        assertEquals("input3 is filled", "setForm2input3:1", messages);
+
+        // fill form3 input1
+        input1 = (HtmlTextInput) page.getElementById("form3:inputs:input1");
+        input1.setValueAttribute("1");
+        input1.fireEvent("change");
+        webClient.waitForBackgroundJavaScript(3000);
+        input1 = (HtmlTextInput) page.getElementById("form3:inputs:input1");
+        input2 = (HtmlTextInput) page.getElementById("form3:inputs:input2");
+        input3 = (HtmlTextInput) page.getElementById("form3:inputs:input3");
+        messages = page.getElementById("form3:messages").asText();
+        assertEquals("input1 is refreshed to 1x", "1x", input1.getValueAttribute());
+        assertEquals("input2 is refreshed to x", "x", input2.getValueAttribute());
+        assertEquals("input3 is empty", "", input3.getValueAttribute());
+        assertEquals("input1 is filled and input2 is empty", "setForm3input1:1\nsetForm3input2:", messages);
+
+        // fill form3 input2
+        input2 = (HtmlTextInput) page.getElementById("form3:inputs:input2");
+        input2.setValueAttribute("1");
+        input2.fireEvent("change");
+        webClient.waitForBackgroundJavaScript(3000);
+        input1 = (HtmlTextInput) page.getElementById("form3:inputs:input1");
+        input2 = (HtmlTextInput) page.getElementById("form3:inputs:input2");
+        input3 = (HtmlTextInput) page.getElementById("form3:inputs:input3");
+        messages = page.getElementById("form3:messages").asText();
+        assertEquals("input1 is refreshed to 1xx", "1xx", input1.getValueAttribute());
+        assertEquals("input2 is refreshed to 1x", "1x", input2.getValueAttribute());
+        assertEquals("input3 is empty", "", input3.getValueAttribute());
+        assertEquals("input1 is filled and input2 is filled", "setForm3input1:1x\nsetForm3input2:1", messages);
+
+        // fill form3 input3
+        input3 = (HtmlTextInput) page.getElementById("form3:inputs:input3");
+        input3.setValueAttribute("1");
+        input3.fireEvent("change");
+        webClient.waitForBackgroundJavaScript(3000);
+        input1 = (HtmlTextInput) page.getElementById("form3:inputs:input1");
+        input2 = (HtmlTextInput) page.getElementById("form3:inputs:input2");
+        input3 = (HtmlTextInput) page.getElementById("form3:inputs:input3");
+        messages = page.getElementById("form3:messages").asText();
+        assertEquals("input1 is refreshed to empty string", "", input1.getValueAttribute());
+        assertEquals("input2 is refreshed to empty string", "", input2.getValueAttribute());
+        assertEquals("input3 is filled and refreshed", "1x", input3.getValueAttribute());
+        assertEquals("input3 is filled", "setForm3input3:1", messages);
     }
 
     private void validateRenderedMarkup(HtmlPage page, String formId, String render) {
@@ -79,7 +206,7 @@ public class Spec1567IT {
         assertEquals("input1 and input2 are implied as @this", "mojarra.ab(this,event,'change','" + formId + ":inputs:input1 " + formId + ":inputs:input2','" + render + "')", input2.getOnChangeAttribute());
 
         HtmlTextInput input3 = (HtmlTextInput) page.getElementById(formId + ":inputs:input3");
-        assertEquals("input3 has still its own f:ajax", "mojarra.ab(this,event,'valueChange',0,'" + formId + ":@form')", input3.getOnChangeAttribute());
+        assertEquals("input3 has still its own f:ajax", "mojarra.ab(this,event,'valueChange',0,'@form')", input3.getOnChangeAttribute());
     }
 
 }
