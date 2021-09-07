7934297d50749cbc910e69c6ab20f6c663ad8079
arjantijms <arjan.tijms@gmail.com>
2021-09-07 22:58:17 +0200

    Enhance tests:
    * Add debug options for the tests
    * Depend on Faces API from project
    * Allow to override location when GF is unpacked
    
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

---
+++ test2/util/src/main/java/com/sun/faces/test/htmlunit/DebugOptions.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 Contributors to Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package com.sun.faces.test.htmlunit;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+
+public class DebugOptions {
+
+    public static void setDebugOptions(WebClient webClient) {
+        if (System.getProperty("suspend") != null) {
+            webClient.getOptions().setTimeout(0);
+        }
+    }
+
+}
