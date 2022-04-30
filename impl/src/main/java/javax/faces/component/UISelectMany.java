4da8a30bbdfd1ca7b8812e7b01801cc74bfaeeea
Bauke Scholtz <balusc@gmail.com>
2022-04-30 10:26:28 -0400

    Fix #5081: UISelectMany#getSubmittedValue() should not default to empty string

--- impl/src/main/java/javax/faces/component/UISelectMany.java
+++ impl/src/main/java/javax/faces/component/UISelectMany.java
@@ -27,6 +27,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.el.ValueBinding;
+import javax.faces.render.Renderer;
 
 
 /**
@@ -261,6 +262,26 @@ public class UISelectMany extends UIInput {
 
     }
 
+
+    private transient Object submittedValue = null;
+
+
+    @Override
+    public Object getSubmittedValue() {
+        if (submittedValue == null && !isValid() && considerEmptyStringNull(FacesContext.getCurrentInstance())) { // JAVASERVERFACES_SPEC_PUBLIC-671
+            return new String[0]; // Mojarra#5081
+        } else {
+            return submittedValue;
+        }
+    }
+
+
+    @Override
+    public void setSubmittedValue(Object submittedValue) {
+
+        this.submittedValue = submittedValue;
+
+    }
 
     /**
      * <p>Return the currently selected values, or <code>null</code> if there
