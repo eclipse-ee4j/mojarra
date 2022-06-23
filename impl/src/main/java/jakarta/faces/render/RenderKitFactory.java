f58360ace5a1d0df7e01a45129be231094801ddf
pizzi80 <paolo@given2.com>
2022-06-23 16:38:55 +0200

    Revert "- removed Deprecated default Constructor"
    This reverts commit 2a16addcea5218a566ed2beb9e85ed2b0a7e16de.

--- impl/src/main/java/jakarta/faces/render/RenderKitFactory.java
+++ impl/src/main/java/jakarta/faces/render/RenderKitFactory.java
@@ -47,7 +47,15 @@ import jakarta.faces.context.FacesContext;
 
 public abstract class RenderKitFactory implements FacesWrapper<RenderKitFactory> {
 
-    private final RenderKitFactory wrapped;
+    private RenderKitFactory wrapped;
+
+    /**
+     * @deprecated Use the other constructor taking the implementation being wrapped.
+     */
+    @Deprecated
+    public RenderKitFactory() {
+
+    }
 
     /**
      * <p class="changed_added_2_3">
