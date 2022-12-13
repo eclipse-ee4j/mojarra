ba403c19446e31939405aece0fc1d2e1448b57b5
Bauke Scholtz <balusc@gmail.com>
2022-12-13 16:43:13 -0400

    https://github.com/jakartaee/faces/issues/1760 - add back id attribute to VDL of h:head and h:body - fix HeadRenderer to only output it when actually specified - fix BodyRenderer to only output it when HTML5 doctype is used

--- impl/src/main/java/com/sun/faces/renderkit/html_basic/BodyRenderer.java
+++ impl/src/main/java/com/sun/faces/renderkit/html_basic/BodyRenderer.java
@@ -54,7 +54,9 @@ public class BodyRenderer extends HtmlBasicRenderer {
     public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
         ResponseWriter writer = context.getResponseWriter();
         writer.startElement("body", component);
-        writeIdAttributeIfNecessary(context, writer, component);
+        if (RenderKitUtils.isOutputHtml5Doctype(context)) {
+            writeIdAttributeIfNecessary(context, writer, component);
+        }
         String styleClass = (String) component.getAttributes().get("styleClass");
         if (styleClass != null && styleClass.length() != 0) {
             writer.writeAttribute("class", styleClass, "styleClass");
