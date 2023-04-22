d12c8f5925258534b7ae6411bab5378ac36675e6
Arjan Tijms <arjan.tijms@omnifish.ee>
2023-04-22 23:14:23 +0200

    Use delegation for UIOutput
    Signed-off-by: Arjan Tijms <arjan.tijms@omnifish.ee>

--- impl/src/main/java/com/sun/faces/api/component/UIOutputImpl.java
+++ impl/src/main/java/com/sun/faces/api/component/UIOutputImpl.java
@@ -14,10 +14,14 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-package jakarta.faces.component;
+package com.sun.faces.api.component;
 
-import com.sun.faces.api.component.UIOutputImpl;
-
+import jakarta.faces.component.PartialStateHolder;
+import jakarta.faces.component.StateHolder;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.component.UIComponentBase;
+import jakarta.faces.component.UIOutput;
+import jakarta.faces.component.ValueHolder;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.convert.Converter;
 
@@ -52,38 +56,55 @@ import jakarta.faces.convert.Converter;
  * be changed by calling the <code>setRendererType()</code> method.
  * </p>
  */
-public class UIOutput extends UIComponentBase implements ValueHolder {
+public class UIOutputImpl extends UIComponentBaseImpl implements ValueHolder {
 
     // ------------------------------------------------------ Manifest Constants
 
     /**
+     * <p>
      * The standard component type for this component.
+     * </p>
      */
     public static final String COMPONENT_TYPE = "jakarta.faces.Output";
 
     /**
+     * <p>
      * The standard component family for this component.
+     * </p>
      */
     public static final String COMPONENT_FAMILY = "jakarta.faces.Output";
 
+    private static final String FORCE_FULL_CONVERTER_STATE = "com.sun.faces.component.UIOutput.forceFullConverterState";
 
-    UIOutputImpl uiOutputImpl;
+    enum PropertyKeys {
+        value, converter
+    }
+
+    private Converter<?> converter;
+
+    UIOutput peer;
 
     // ------------------------------------------------------------ Constructors
 
+    @Override
+    public UIOutput getPeer() {
+        return peer;
+    }
+
+    public void setPeer(UIOutput peer) {
+        this.peer = peer;
+        super.setPeer(peer);
+    }
 
     /**
      * <p>
      * Create a new {@link UIOutput} instance with default property values.
      * </p>
      */
-    public UIOutput() {
-        super(new UIOutputImpl());
+    public UIOutputImpl() {
+        super();
         setRendererType("jakarta.faces.Text");
-        this.uiOutputImpl = (UIOutputImpl) getUiComponentBaseImpl();
-        uiOutputImpl.setPeer(this);
     }
-
 
     // -------------------------------------------------------------- Properties
 
@@ -96,17 +117,26 @@ public class UIOutput extends UIComponentBase implements ValueHolder {
 
     @Override
     public Converter getConverter() {
-        return uiOutputImpl.getConverter();
+        if (converter != null) {
+            return converter;
+        }
+
+        return (Converter) getStateHelper().eval(PropertyKeys.converter);
     }
 
     @Override
     public void setConverter(Converter converter) {
-        uiOutputImpl.setConverter(converter);
+        clearInitialState();
+
+        // we don't push the converter to the StateHelper
+        // if it's been explicitly set (i.e. it's not a ValueExpression
+        // reference
+        this.converter = converter;
     }
 
     @Override
     public Object getLocalValue() {
-        return uiOutputImpl.getLocalValue();
+        return getStateHelper().get(PropertyKeys.value);
     }
 
     /**
@@ -118,12 +148,12 @@ public class UIOutput extends UIComponentBase implements ValueHolder {
      */
     @Override
     public Object getValue() {
-        return uiOutputImpl.getValue();
+        return getStateHelper().eval(PropertyKeys.value);
     }
 
     @Override
     public void setValue(Object value) {
-        uiOutputImpl.setValue(value);
+        getStateHelper().put(PropertyKeys.value, value);
     }
 
     /**
@@ -134,31 +164,103 @@ public class UIOutput extends UIComponentBase implements ValueHolder {
      * @since 2.2
      */
     public void resetValue() {
-        uiOutputImpl.resetValue();
+        getStateHelper().remove(PropertyKeys.value);
     }
 
     /**
+     * <p>
      * In addition to the actions taken in {@link UIComponentBase} when {@link PartialStateHolder#markInitialState()} is
      * called, check if the installed {@link Converter} is a PartialStateHolder and if it is, call
      * {@link jakarta.faces.component.PartialStateHolder#markInitialState()} on it.
+     * </p>
      */
     @Override
     public void markInitialState() {
-        uiOutputImpl.markInitialState();
+        super.markInitialState();
+
+        Converter<?> c = getConverter();
+        if (c instanceof PartialStateHolder) {
+            ((PartialStateHolder) c).markInitialState();
+        }
     }
 
     @Override
     public void clearInitialState() {
-        uiOutputImpl.clearInitialState();
+        if (initialStateMarked()) {
+            super.clearInitialState();
+
+            Converter<?> c = getConverter();
+            if (c != null && c instanceof PartialStateHolder) {
+                ((PartialStateHolder) c).clearInitialState();
+            }
+        }
     }
 
     @Override
     public Object saveState(FacesContext context) {
-        return uiOutputImpl.saveState(context);
+        if (context == null) {
+            throw new NullPointerException();
+        }
+
+        Object converterState = null;
+        if (converter != null) {
+            if (!initialStateMarked() || getAttributes().containsKey(FORCE_FULL_CONVERTER_STATE)) {
+                /*
+                 * Check if our parent component has its initial state marked and we know we don't. That means we are not using the same
+                 * state saving algorithm. So we are going to ALWAYS force to do FSS for the converter.
+                 */
+                if (getParent() != null && getParent().initialStateMarked()) {
+                    getAttributes().put(FORCE_FULL_CONVERTER_STATE, true);
+                    if (converter instanceof PartialStateHolder) {
+                        PartialStateHolder partialStateHolder = (PartialStateHolder) converter;
+                        partialStateHolder.clearInitialState();
+                    }
+                }
+
+                converterState = saveAttachedState(context, converter);
+            } else {
+                if (converter instanceof StateHolder) {
+                    StateHolder stateHolder = (StateHolder) converter;
+                    if (!stateHolder.isTransient()) {
+                        converterState = ((StateHolder) converter).saveState(context);
+                    }
+                }
+            }
+        }
+
+        Object[] values = (Object[]) super.saveState(context);
+
+        if (converterState != null || values != null) {
+            return new Object[] { values, converterState };
+        }
+
+        return values;
     }
 
     @Override
     public void restoreState(FacesContext context, Object state) {
-        uiOutputImpl.restoreState(context, state);
+
+        if (context == null) {
+            throw new NullPointerException();
+        }
+
+        if (state == null) {
+            return;
+        }
+
+        Object[] values = (Object[]) state;
+        super.restoreState(context, values[0]);
+        Object converterState = values[1];
+        if (converterState instanceof StateHolderSaver) {
+            // this means full state was saved and as such
+            // overwrite any existing converter with the saved
+            // converter
+            converter = (Converter<?>) restoreAttachedState(context, converterState);
+        } else {
+            // apply any saved state to the existing converter
+            if (converterState != null && converter instanceof StateHolder) {
+                ((StateHolder) converter).restoreState(context, converterState);
+            }
+        }
     }
 }
