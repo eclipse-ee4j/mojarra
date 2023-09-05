ba5016d7639c704d8720350b9beb380f4dd3d3ac
pizzi80 <paolo@given2.com>
2023-09-05 19:41:02 +0200

    Faces Standard Converters with Generics
    Signed-off-by: pizzi80 <paolo@given2.com>

--- impl/src/main/java/com/sun/faces/util/Util.java
+++ impl/src/main/java/com/sun/faces/util/Util.java
@@ -53,6 +53,7 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.logging.Level;
