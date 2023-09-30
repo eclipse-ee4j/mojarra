d77e716fdaf675539f7b61cfba8aad22f4d46b96
Bauke Scholtz <balusc@gmail.com>
2023-09-30 16:07:02 -0400

    Fixed compilation error caused by accidental usage of Java16 method https://github.com/jakartaee/faces/issues/1507 -- we still need to bump Java version of Faces 5.0 project to align with Jakarta EE 11

--- impl/src/main/java/jakarta/faces/component/html/HtmlEvents.java
+++ impl/src/main/java/jakarta/faces/component/html/HtmlEvents.java
@@ -1,9 +1,12 @@
 package jakarta.faces.component.html;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import jakarta.faces.context.FacesContext;
@@ -108,8 +111,8 @@ public final class HtmlEvents {
      */
     public static final String ADDITIONAL_HTML_EVENT_NAMES_PARAM_NAME = "jakarta.faces.ADDITIONAL_HTML_EVENT_NAMES";
 
-    private static final Collection<String> DOCUMENT_ELEMENT_EVENT_NAMES = Arrays.stream(DocumentElementEvent.values()).map(e -> e.name()).sorted().distinct().toList();
-    private static final Collection<String> BODY_ELEMENT_EVENT_NAMES = Stream.concat(DOCUMENT_ELEMENT_EVENT_NAMES.stream(), Arrays.stream(BodyElementEvent.values()).map(e -> e.name())).sorted().distinct().toList();
+    private static final Collection<String> DOCUMENT_ELEMENT_EVENT_NAMES = Arrays.stream(DocumentElementEvent.values()).map(e -> e.name()).sorted().distinct().collect(toList());
+    private static final Collection<String> BODY_ELEMENT_EVENT_NAMES = Stream.concat(DOCUMENT_ELEMENT_EVENT_NAMES.stream(), Arrays.stream(BodyElementEvent.values()).map(e -> e.name())).sorted().distinct().collect(toList());
 
     private HtmlEvents() {
         throw new AssertionError();
@@ -135,8 +138,8 @@ public final class HtmlEvents {
     private static Collection<String> mergeAdditionalHtmlEventNamesIfNecessary(FacesContext context, String applicationKey, Collection<String> documentEventNames, Collection<String> bodyEventNames) {
         return (Collection<String>) context.getExternalContext().getApplicationMap().computeIfAbsent(applicationKey, $ -> {
             String[] additionalHtmlEventNames = Optional.ofNullable(context.getExternalContext().getInitParameter(ADDITIONAL_HTML_EVENT_NAMES_PARAM_NAME)).map(p -> p.split("\\s+")).orElse(new String[0]);
-            return Stream.concat(Stream.concat(documentEventNames.stream(), bodyEventNames.stream()), Arrays.stream(additionalHtmlEventNames)).sorted().distinct().toList();
+            return Stream.concat(Stream.concat(documentEventNames.stream(), bodyEventNames.stream()), Arrays.stream(additionalHtmlEventNames)).sorted().distinct().collect(toList());
         });
     }
-
+    
 }
