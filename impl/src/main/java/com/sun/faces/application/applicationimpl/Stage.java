910947623b3c1d62bfec87cd995ff84e36344f99
Bauke Scholtz <balusc@gmail.com>
2023-10-07 17:28:59 -0400

    Fixed build error

--- impl/src/main/java/com/sun/faces/application/applicationimpl/Stage.java
+++ impl/src/main/java/com/sun/faces/application/applicationimpl/Stage.java
@@ -17,9 +17,7 @@
 package com.sun.faces.application.applicationimpl;
 
 import static jakarta.faces.application.ProjectStage.Development;
-import static jakarta.faces.application.ProjectStage.Production;
 import static java.util.logging.Level.FINE;
-import static java.util.logging.Level.INFO;
 
 import java.util.logging.Logger;
 
@@ -45,9 +43,7 @@ public class Stage {
     public ProjectStage getProjectStage(Application application) {
 
         if (projectStage == null) {
-            String value = fetchProjectStageFromConfig();
-
-            setProjectStageFromValue(value, Production);
+            projectStage = fetchProjectStageFromConfig();
 
             if (projectStage == Development) {
                 application.subscribeToEvent(PostAddToViewEvent.class, new ValidateComponentNesting());
@@ -59,38 +55,24 @@ public class Stage {
 
     // ----------------------------------------------------------- Private methods
 
-    private String fetchProjectStageFromConfig() {
-        WebConfiguration webConfig = WebConfiguration.getInstance(FacesContext.getCurrentInstance().getExternalContext());
+    private ProjectStage fetchProjectStageFromConfig() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        WebConfiguration webConfig = WebConfiguration.getInstance(context.getExternalContext());
         String value = webConfig.getEnvironmentEntry(WebConfiguration.WebEnvironmentEntry.ProjectStage);
 
         if (value != null) {
+            projectStage = ProjectStage.valueOf(value);
             if (LOGGER.isLoggable(FINE)) {
                 LOGGER.log(FINE, "ProjectStage configured via JNDI: {0}", value);
             }
         } else {
-            value = ContextParam.PROJECT_STAGE.getValue(FacesContext.getCurrentInstance());
-            if (value != null && LOGGER.isLoggable(FINE)) {
+            projectStage = ContextParam.PROJECT_STAGE.getValue(context);
+            if (LOGGER.isLoggable(FINE)) {
                 LOGGER.log(FINE, "ProjectStage configured via servlet context init parameter: {0}", value);
             }
         }
 
-        return value;
-    }
-
-    private void setProjectStageFromValue(String value, ProjectStage defaultStage) {
-        if (value != null) {
-            try {
-                projectStage = ProjectStage.valueOf(value);
-            } catch (IllegalArgumentException iae) {
-                if (LOGGER.isLoggable(INFO)) {
-                    LOGGER.log(INFO, "Unable to discern ProjectStage for value {0}.", value);
-                }
-            }
-        }
-
-        if (projectStage == null) {
-            projectStage = defaultStage;
-        }
+        return projectStage;
     }
 
 }
