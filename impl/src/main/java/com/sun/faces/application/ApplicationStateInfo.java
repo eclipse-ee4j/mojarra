bd467e4083e8f779dde198da5b084c07d8ef35a0
Bauke Scholtz <balusc@gmail.com>
2023-10-15 10:44:06 -0400

    Faces 4.1: deprecate FSS #5337 https://github.com/jakartaee/faces/issues/1829

--- impl/src/main/java/com/sun/faces/application/ApplicationStateInfo.java
+++ impl/src/main/java/com/sun/faces/application/ApplicationStateInfo.java
@@ -23,14 +23,20 @@ import static java.util.Arrays.asList;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Logger;
 
 import com.sun.faces.config.WebConfiguration;
+import com.sun.faces.util.FacesLogger;
+
+import jakarta.faces.application.StateManager;
 
 /**
  * This class maintains per-application information pertaining to partail or full state saving as a whole or partial
  * state saving with some views using full state saving.
  */
 public class ApplicationStateInfo {
+
+    private static final Logger LOGGER = FacesLogger.APPLICATION.getLogger();
 
     private final boolean partialStateSaving;
     private Set<String> fullStateViewIds;
@@ -46,6 +52,10 @@ public class ApplicationStateInfo {
             String[] viewIds = config.getOptionValue(FullStateSavingViewIds, ",");
             fullStateViewIds = new HashSet<>(viewIds.length, 1.0f);
             fullStateViewIds.addAll(asList(viewIds));
+        }
+        else {
+            LOGGER.warning("The configuration '" + StateManager.PARTIAL_STATE_SAVING_PARAM_NAME
+                + "' is deprecated as of Faces 4.1 and should not longer be used.");
         }
 
     }
