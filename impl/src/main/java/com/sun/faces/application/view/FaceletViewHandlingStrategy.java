9928f2354686dbac26c45872cdf8e18241dc85ee
Bauke Scholtz <balusc@gmail.com>
2025-03-01 12:25:16 -0400

    fix javadoc error

--- impl/src/main/java/com/sun/faces/application/view/FaceletViewHandlingStrategy.java
+++ impl/src/main/java/com/sun/faces/application/view/FaceletViewHandlingStrategy.java
@@ -771,9 +771,6 @@ public class FaceletViewHandlingStrategy extends ViewHandlingStrategy {
      * @param viewId the view ID to check
      * @return <code>true</code> if assuming a default configuration and the view ID's extension in {@link ViewHandler#FACELETS_SUFFIX_PARAM_NAME}
      * Otherwise try to match the view ID based on the configured extensions and prefixes in {@link ViewHandler#FACELETS_VIEW_MAPPINGS_PARAM_NAME}
-     *
-     * @see ContextParam#FACELETS_SUFFIX
-     * @see ContextParam#FACELETS_VIEW_MAPPINGS
      */
     @Override
     public boolean handlesViewId(String viewId) {
