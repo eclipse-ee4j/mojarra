8a9c8eb254f54ac8687eee49e4318854208c08a9
Bauke Scholtz <balusc@gmail.com>
2025-08-04 10:08:45 -0400

    add IT to prove #5596

---
+++ test/issue5596/src/main/java/org/eclipse/mojarra/test/issue5596/Issue5596.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GPL-2.0 with Classpath-exception-2.0 which
+ * is available at https://openjdk.java.net/legal/gplv2+ce.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 or Apache-2.0
+ */
+package org.eclipse.mojarra.test.issue5596;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class Issue5596 {
+
+    private Byte myByte;
+    private Short myShort;
+    private Integer myInteger;
+    private Long myLong;
+    private Float myFloat;
+    private Double myDouble;
+    private BigInteger bigInteger;
+    private BigDecimal bigDecimal;
+
+    public Byte getMyByte() {
+        return myByte;
+    }
+
+    public void setMyByte(Byte myByte) {
+        this.myByte = myByte;
+    }
+
+    public Short getMyShort() {
+        return myShort;
+    }
+
+    public void setMyShort(Short myShort) {
+        this.myShort = myShort;
+    }
+
+    public Integer getMyInteger() {
+        return myInteger;
+    }
+
+    public void setMyInteger(Integer myInteger) {
+        this.myInteger = myInteger;
+    }
+
+    public Long getMyLong() {
+        return myLong;
+    }
+
+    public void setMyLong(Long myLong) {
+        this.myLong = myLong;
+    }
+
+    public Float getMyFloat() {
+        return myFloat;
+    }
+    
+    public void setMyFloat(Float myFloat) {
+        this.myFloat = myFloat;
+    }
+    
+    public Double getMyDouble() {
+        return myDouble;
+    }
+
+    public void setMyDouble(Double myDouble) {
+        this.myDouble = myDouble;
+    }
+
+    public BigInteger getBigInteger() {
+        return bigInteger;
+    }
+
+    public void setBigInteger(BigInteger bigInteger) {
+        this.bigInteger = bigInteger;
+    }
+
+    public BigDecimal getBigDecimal() {
+        return bigDecimal;
+    }
+
+    public void setBigDecimal(BigDecimal bigDecimal) {
+        this.bigDecimal = bigDecimal;
+    }
+
+}
