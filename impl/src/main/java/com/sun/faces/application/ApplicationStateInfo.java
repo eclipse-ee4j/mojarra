a49b6dae27ce1ce3005893c5b16c94798904858e
pizzi80 <paolo@given2.com>
2026-09-02 13:16:51 +0200

    ApplicationStateInfo: optimizations
    Signed-off-by: pizzi80 <paolo@given2.com>

--- impl/src/main/java/com/sun/faces/application/ApplicationStateInfo.java
+++ impl/src/main/java/com/sun/faces/application/ApplicationStateInfo.java
@@ -19,9 +19,8 @@ package com.sun.faces.application;
 import static com.sun.faces.config.WebConfiguration.BooleanWebContextInitParameter.PartialStateSaving;
 import static com.sun.faces.config.WebConfiguration.WebContextInitParameter.FullStateSavingViewIds;
 import static com.sun.faces.util.Util.notNullViewId;
-import static java.util.Arrays.asList;
 
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.Set;
 import java.util.logging.Logger;
 
@@ -31,7 +30,7 @@ import com.sun.faces.util.FacesLogger;
 import jakarta.faces.application.StateManager;
 
 /**
- * This class maintains per-application information pertaining to partail or full state saving as a whole or partial
+ * This class maintains per-application information pertaining to partial or full state saving as a whole or partial
  * state saving with some views using full state saving.
  */
 public class ApplicationStateInfo {
@@ -39,7 +38,7 @@ public class ApplicationStateInfo {
     private static final Logger LOGGER = FacesLogger.APPLICATION.getLogger();
 
     private final boolean partialStateSaving;
-    private Set<String> fullStateViewIds;
+    private final Set<String> fullStateViewIds;
 
     // ------------------------------------------------------------ Constructors
 
@@ -49,11 +48,11 @@ public class ApplicationStateInfo {
         partialStateSaving = config.isOptionEnabled(PartialStateSaving);
 
         if (partialStateSaving) {
-            String[] viewIds = config.getOptionValue(FullStateSavingViewIds, ',');
-            fullStateViewIds = new HashSet<>(viewIds.length, 1.0f);
-            fullStateViewIds.addAll(asList(viewIds));
+            fullStateViewIds = Set.of(config.getOptionValue(FullStateSavingViewIds, ','));
         }
         else {
+            fullStateViewIds = Collections.emptySet();
+
             LOGGER.warning("The configuration '" + StateManager.PARTIAL_STATE_SAVING_PARAM_NAME
                 + "' is deprecated as of Faces 4.1 and should not longer be used.");
         }
